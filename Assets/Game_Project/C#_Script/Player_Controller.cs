980dae691e6674c4211f187b76854f0a60645b2c
Maguslibratis162 <51017800+Maguslibratis162@users.noreply.github.com>
2024-02-24 17:39:07 +0700

    Update

--- Assets/Game_Project/C#_Script/Player_Controller.cs
+++ Assets/Game_Project/C#_Script/Player_Controller.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using FishNet.Object;
 using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 
 public class Player_Controller : NetworkBehaviour
 {
@@ -11,6 +12,16 @@ public class Player_Controller : NetworkBehaviour
     private Vector3 direction;
     [SerializeField]
     private CharacterController characterController;
+
+    [SerializeField]
+    private float ground_Y_Offset;
+    [SerializeField]
+    private LayerMask groundMask;
+    private Vector3 SpherePos;
+
+    [SerializeField]
+    private float gravity = -9.8f;
+    private Vector3 Velocity;
     public override void OnStartClient()
     {
         base.OnStartClient();
@@ -32,7 +43,9 @@ public class Player_Controller : NetworkBehaviour
     // Update is called once per frame
     void Update()
     {
+        //if(Input_Controller.instance.MoveAction.IsPressed())
         Movement();
+        Gravity();
     }
 
     private void Movement()
@@ -41,5 +54,23 @@ public class Player_Controller : NetworkBehaviour
         input = Input_Controller.instance.MoveAction.ReadValue<Vector2>(); ;
         direction = transform.forward * input.y +transform.right*input.x ;
         characterController.Move(direction* moveSpeed*Time.deltaTime);
+    }
+    private bool IsGrounded()
+    {
+        SpherePos = new Vector3(transform.position.x, transform.position.y - ground_Y_Offset, transform.position.z);
+        if (Physics.CheckSphere(SpherePos, characterController.radius - 0.05f, groundMask)) { return true; }
+        return false;
+    }
+    private void Gravity()
+    {
+        if (!IsGrounded()) Velocity.y += gravity * Time.deltaTime;
+        else if (Velocity.y < 0) Velocity.y = -2;
+
+        characterController.Move(Velocity * Time.deltaTime);
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(SpherePos, characterController.radius - 0.05f);
     }
 }
